ea035c9fbdbf2fe420b722fcc8a12149e202af2e
Rajat Silwal <rajatsilwal7@gmail.com>
2020-04-28 00:06:21 +0530

    prototypical inheritance task4

--- jsPractice/soution1.js
+++ jsPractice/soution1.js
@@ -308,9 +308,101 @@ clock.start();
 
 
 
+// inheritance in javascript task.
+
+class Animal {
+
+  constructor(name) {
+    this.name = name;
+  }
+
+}
+
+class Rabbit extends Animal {
+  constructor(name) {
+  	super();
+    this.name = name;
+    this.created = Date.now();
+  }
+}
+
+let rabbit = new Rabbit("White Rabbit"); 
+alert(rabbit.name);
+
+
+//--------------------------------------------------------------------
+
+class Clock {
+  constructor({ template }) {
+    this.template = template;
+  }
+
+  render() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    let mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+
+    let secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+
+    let output = this.template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+
+    console.log(output);
+  }
+
+  stop() {
+    clearInterval(this.timer);
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), 1000);
+  }
+}
 
 
 
+// class to add the percision variable 
+
+class ExtendedClock extends Clock {
+  constructor(options) {
+    super(options);
+    let { precision = 1000 } = options;
+    this.precision = precision;
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), this.precision);
+  }
+};
+
+//----------------------------------------------------------------------------
+
+class Rabbit extends Object {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+let rabbit = new Rabbit("Rab");
+
+alert( rabbit.hasOwnProperty('name') ); 
 
 
+// instance of task 
+function A() {}
+function B() {}
 
+A.prototype = B.prototype = {};
+
+let a = new A();
+
+alert( a instanceof B ); // true
